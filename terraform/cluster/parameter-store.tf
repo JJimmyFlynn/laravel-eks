bf3bc9bc11b03fb22992aa7992f231871184e2e1
John Flynn <jjimmyflynn@gmail.com>
2025-02-10 03:34:04 -0500

    Add additional env variables to secrets class and laravel deploy

--- terraform/cluster/parameter-store.tf
+++ terraform/cluster/parameter-store.tf
@@ -1,4 +1,36 @@
-# resource "aws_ssm_parameter" "" {
-#   name = "${var.parameter_store_path}/DB_"
-#   type = ""
-# }
+resource "aws_ssm_parameter" "env_app_name" {
+  name = "${var.parameter_store_path}/APP_NAME"
+  type = "String"
+  value = "laravel-k8s"
+}
+
+resource "aws_ssm_parameter" "env_app_environment" {
+  name = "${var.parameter_store_path}/APP_ENV"
+  type = "String"
+  value = "dev"
+}
+
+resource "aws_ssm_parameter" "env_db_connection" {
+  name = "${var.parameter_store_path}/DB_CONNECTION"
+  type = "String"
+  value = "mysql"
+}
+
+resource "aws_ssm_parameter" "env_db_host" {
+  name = "${var.parameter_store_path}/DB_HOST"
+  type = "String"
+  value = aws_rds_cluster.default.endpoint
+}
+
+resource "aws_ssm_parameter" "env_db_username" {
+  name = "${var.parameter_store_path}/DB_USERNAME"
+  type = "String"
+  value = aws_rds_cluster.default.master_username
+}
+
+resource "aws_ssm_parameter" "env_db_database" {
+  name = "${var.parameter_store_path}/DB_DATABASE"
+  type = "String"
+  value = aws_rds_cluster.default.database_name
+}
+
